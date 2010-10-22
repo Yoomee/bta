116990e32e30c0c8c3320c9fceea82c5c6eb80f1
Ian Mooney <ianmooney@ian-mooneys-imac-6.local>
2010-10-22 15:13:24 +0100

    changes to before_migrate

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,14 +1,15 @@
-# Stop old workling client if exists
 run "cd #{release_path} && sudo ext update"
+run "sudo touch #{shared_path}/log/production.log"
+run "sudo chown deploy:deploy #{shared_path}/log/production.log"
+run "cd #{release_path} && sudo rake gems:install RAILS_ENV=production -t"
+
+# Stop old workling client if exists
 run "cd #{current_path} && sudo RAILS_ENV=production script/workling_client stop" if File.exists?("#{current_path}/log/workling.pid")
 # Start starling
 run "cd #{release_path} && sudo starling -d -p 15151"
 # Start new workling client
 run "cd #{release_path} && sudo RAILS_ENV=production script/workling_client start"
 
-run "sudo touch #{shared_path}/log/production.log"
-run "sudo chown deploy:deploy #{shared_path}/log/production.log"
-run "cd #{release_path} && sudo rake gems:install RAILS_ENV=production -t"
 db_yaml_path = "#{shared_path}/config/database.yml"
 db_yaml = YAML.load_file(db_yaml_path)
 db_yaml['production']['reconnect'] = true
