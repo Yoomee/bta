2311bd575fe786e841d1e6eea22ee7072c1848e9
Ian Mooney <ianmooney@ian-mooneys-imac-7.local>
2010-10-27 16:04:42 +0100

    wired in facebook_connect

--- client/app/models/contact.rb
+++ client/app/models/contact.rb
@@ -1,15 +1,13 @@
 class Contact < ActiveRecord::Base
-  
-  EMAIL_FORMAT = /^[^\s]+@[^\s]+\.[a-z]{2,}$/
-  URL_FORMAT = /((((file|gopher|news|nntp|telnet|http|ftp|https|ftps|sftp):\/\/)|(www\.))+(([a-zA-Z0-9\._-]+\.[a-zA-Z]{2,6})|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(\/[a-zA-Z0-9\&amp;%_\.\/-~-]*)?(?!['"]))|^\s*$/
 
-  belongs_to :category, :class_name => 'ContactCategory'
-  has_location  
   include TramlinesImages
   
+  belongs_to :category, :class_name => 'ContactCategory'
+  has_location  
+  
   validates_presence_of :name
-  validates_format_of :email, :with => EMAIL_FORMAT, :allow_blank => true
-  validates_format_of :website, :with => URL_FORMAT, :allow_blank => true
+  validates_email_format_of :email, :allow_blank => true
+  validates_url_format_of :website, :allow_blank => true
   
   alias_attribute :address, :location
   
