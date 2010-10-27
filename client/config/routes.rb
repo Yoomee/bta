3c8fd4fbdf34210de15032d4826705e8466e5ec1
Matt Atkins <matt@yoomee.com>
2010-10-27 11:36:36 +0100

    Google maps on directory, autocomplete search.

--- client/config/routes.rb
+++ client/config/routes.rb
@@ -3,4 +3,10 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :contacts
   map.resources :contact_categories
   
+  map.directory '/directory/:id', :controller => 'contacts', :action => 'index'
+  
+  map.resources :searches, :controller => :search, :as => :search, :new => {:autocomplete => :get}
+  
+  
+  
 end
