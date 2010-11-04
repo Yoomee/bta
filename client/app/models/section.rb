5a605fe1e26133d0d2cf263c2cbd71e176e01a7f
Rich Wells <rich@yoomee.com>
2010-11-04 17:41:28 +0000

    changes to submenu

--- client/app/models/section.rb
+++ client/app/models/section.rb
@@ -2,4 +2,14 @@ Section.class_eval do
   
   alias_attribute :title, :name
   
+  class << self
+    
+    def news
+      s = Section.find_or_initialize_by_slug("news_and_events")
+      s.update_attribute(:name, "News & Events") if s.new_record?
+      s
+    end
+    
+  end
+  
 end
