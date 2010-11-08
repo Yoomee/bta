1b2d2ab235944afabac33acaaa33ffe33ecd2a76
Matt Atkins <matt@yoomee.com>
2010-11-08 09:35:56 +0000

    Facebook and twitter on forums show.

---
+++ client/app/controllers/forums_controller.rb
@@ -0,0 +1,10 @@
+ForumsController.class_eval do
+  
+  before_filter :get_latest_tweet, :only => %w{show}  
+  
+  private
+  def get_latest_tweet
+    @latest_tweet = fetch_latest_tweets_from("britishtinnitus", 1, false, false).first
+  end
+  
+end
