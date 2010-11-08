ee2700cd81d179bceff483461a6613955e7e8be3
Simon Wilkins <simonwilkins@sislocalhost.com>
2010-11-08 17:29:03 +0000

    Added member prices to products

--- client/app/models/product.rb
+++ client/app/models/product.rb
@@ -2,5 +2,14 @@ Product.class_eval do
   
   has_wall
   rateable_by_member
+
+  def member_price_in_pounds
+    return nil if member_price_in_pence.nil?
+    member_price_in_pence.to_f / 100
+  end
   
+  def member_price_in_pounds=(val)
+    self.member_price_in_pence = val.blank? ? nil : val * 100
+  end
+    
 end
