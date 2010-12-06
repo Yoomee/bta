687fe89901cf33a5242579f42f07077a6e5b6e0f
Ian Mooney <ianmooney@ian-mooneys-imac-7.local>
2010-12-06 11:59:37 +0000

    styles for event_calendar added "Login or register" link to forum

--- client/app/models/section.rb
+++ client/app/models/section.rb
@@ -5,9 +5,7 @@ Section.class_eval do
   class << self
     
     def news
-      s = Section.find_or_initialize_by_slug("news_and_events")
-      s.update_attribute(:name, "News & Events") if s.new_record?
-      s
+      slug(:news_and_events, :name => "News & Events")
     end
     
   end
