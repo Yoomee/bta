8580dadad0f200ad7f59db0101e782028101eb37
Si Wilkins <si@yoomee.com>
2010-12-14 10:51:35 +0000

    Added limit to carousel items

--- client/app/controllers/home_controller.rb
+++ client/app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ HomeController.class_eval do
     @latest_news_item = Section.news.pages.latest.first
     @news_items = Section.news.pages.latest.not_including(@latest_news_item).limit(2)
     @products = Product.latest.limit(3)
-    @carousel_pages = Page.show_in_carousel
+    @carousel_pages = Page.show_in_carousel.limit(4)
     @campaign = Section.slug(:campaigns).children.descend_by_created_at.limit(1).first
     @forum_topics = Forum.first.nil? ? [] : Forum.first.topics.descend_by_sticky.limit(3)
   end
