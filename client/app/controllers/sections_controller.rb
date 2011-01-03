e3b7124ca4f5e952c770cb0b6a077e672ef8b8f7
Ian Mooney <ian@yoomee.com>
2011-01-03 10:28:27 +0000

    Added related content to sections#show

--- client/app/controllers/sections_controller.rb
+++ client/app/controllers/sections_controller.rb
@@ -3,14 +3,9 @@ SectionsController.class_eval do
   def show
     case @section.view
       when 'latest_stories'
-        if @section.slug_is("about-tinnitus")
-          @pages_sections = @section.all_children
-        else
-          @pages_sections = @section.pages.published.latest + @section.children
-          @pages_sections.extend(SectionsController::SortByWeightAndLatest)
-          @pages_sections = @pages_sections.sort_by_weight_and_latest
-        end
-        @pages_sections = @pages_sections.paginate(:page => params[:page], :per_page => (APP_CONFIG[:latest_stories_items_per_page] || 6))
+        @pages_sections = @section.pages.published.latest + @section.children
+        @pages_sections.extend(SectionsController::SortByWeightAndLatest)
+        @pages_sections = @pages_sections.sort_by_weight_and_latest.paginate(:page => params[:page], :per_page => (APP_CONFIG[:latest_stories_items_per_page] || 6))
         return render(:action => 'latest_stories')
       when 'first_page'
         redirect_to page_path(@section.first_page) unless @section.pages.empty?
