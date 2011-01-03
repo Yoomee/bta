afa1aa54526f875aceb2fa18b9bd9cdc8c9b4b15
Ian Mooney <ian@yoomee.com>
2011-01-03 17:16:01 +0000

    Removed facebooker from externals and added it as a gem

--- client/config/environment.rb
+++ client/config/environment.rb
@@ -2,7 +2,8 @@ class ClientEnvironment
 
   class << self
     def setup(config)
-      config.gem 'yoomee-searchlogic', :lib => 'searchlogic'      
+      config.gem 'yoomee-searchlogic', :lib => 'searchlogic'
+      config.gem 'facebooker'       
     end
   end
 
