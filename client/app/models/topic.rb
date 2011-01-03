aaafbd3cee217e09b043940ffc97ca8913add1ba
Ian Mooney <ian@yoomee.com>
2011-01-03 12:31:54 +0000

    Cganed member from Ian to Nic to recieve email notifications whenever someone posts in the forum

--- client/app/models/topic.rb
+++ client/app/models/topic.rb
@@ -2,8 +2,8 @@ Topic.class_eval do
   
   # always notifiy nic when anyone posts in the forum
   def notify_others_with_nic post
-    member_nic = Member.find_by_email("ian@yoomee.com")
-    if !post.owned_by?(member_nic) && !posts.exists?(:member_id => member_nic.id, :email => true)
+    member_nic = Member.nic
+    if member_nic && !post.owned_by?(member_nic) && !posts.exists?(:member_id => member_nic.id, :email => true)
       Notifier.deliver_post_notification(member_nic, post)
     end
     notify_others_without_nic(post)
