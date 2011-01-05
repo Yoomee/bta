1bc9ef2264a28d32a934e8540680fbac9a9c4e2d
Si Wilkins <si@yoomee.com>
2011-01-05 10:20:30 +0000

    Added ordering to related documents

--- client/app/controllers/pages_controller.rb
+++ client/app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 PagesController.class_eval do
   def show
-    @related_audio = Document.related_to_page(@page).audio
-    @related_documents = Document.related_to_page(@page).non_audio
+    @related_audio = Document.related_to_page(@page).audio.most_recent
+    @related_documents = Document.related_to_page(@page).non_audio.most_recent
     if @page.is_event?
       @breadcrumb = [Section.find_by_slug("news_and_events"), @page]
     end
