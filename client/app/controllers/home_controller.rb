9a994da3476ba63ed4a5b6a0e28b0fe38ef6605b
Si Wilkins <si@yoomee.com>
2011-01-06 13:07:32 +0000

    Forum changes

--- client/app/controllers/home_controller.rb
+++ client/app/controllers/home_controller.rb
@@ -3,14 +3,14 @@ HomeController.class_eval do
   before_filter :get_latest_tweet, :only => %w{index}
   
   def index
+    @about_tinnitus_page = Page.slug("about_tinnitus")
+    @campaign = Section.slug(:campaigns).children.show_on_front_page.descend_by_created_at.limit(1).first
+    @carousel_pages = Page.show_in_carousel.all(:order => 'created_at DESC', :limit => 5)
     @events = Event.closest.limit(3)
+    @forum_topics = Topic.recently_posted_to.first(3)
     @news_items = Section.latest_news(3)
     @latest_news_item = @news_items.shift
     @products = Product.latest.limit(3)
-    @carousel_pages = Page.show_in_carousel.all(:order => 'created_at DESC', :limit => 5)
-    @campaign = Section.slug(:campaigns).children.show_on_front_page.descend_by_created_at.limit(1).first
-    @forum_topics = Forum.first.nil? ? [] : Forum.first.topics.descend_by_sticky.limit(3)
-    @about_tinnitus_page = Page.slug("about_tinnitus")
     @professionals_section = Section.slug("for-health-professionals")
     @researchers_section = Section.slug("for-researchers")
     @support_and_services_section = Section.slug("support-and-services")
