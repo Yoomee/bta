97e36b84a513b029c63fd40d43ae3193eca161e3
Matt Atkins <matt@yoomee.com>
2011-01-10 12:12:45 +0000

    Show section of page in ck_editor link autocomplete.

--- client/app/controllers/search_controller.rb
+++ client/app/controllers/search_controller.rb
@@ -7,4 +7,12 @@ SearchController.class_eval do
     render :json => results.to_json
   end
   
+  #used in ckeditor link dialog
+  def jquery_autocomplete
+    search = Search.new({:term => params.delete(:q)}, :autocomplete => true)
+    results = search.results.map {|result| {:name => "#{result.to_s} (#{result.class}#{" in <em>#{result.section}</em>" if result.is_a?(Page)})", :url => url_for(result)}}
+    results += search.non_sphinx_results.map {|result| {:name => "#{result.form_title} (Enquiry form)", :url => url_for(:controller => 'enquiries', :action => 'new', :id => result.form_name)}}
+    render :json => results.to_json
+  end
+  
 end
