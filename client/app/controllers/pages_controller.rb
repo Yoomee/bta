bc7d0d660244f6265b8e0f2e638869608b456e83
Matt Atkins <matt@yoomee.com>
2011-01-28 11:14:49 +0000

    Slug fix

--- client/app/controllers/pages_controller.rb
+++ client/app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ PagesController.class_eval do
     @related_audio = Document.related_to_page(@page).audio.most_recent
     @related_documents = Document.related_to_page(@page).non_audio.most_recent
     if @page.is_event?
-      @breadcrumb = [Section.find_by_slug("news_and_events"), @page]
+      @breadcrumb = [Slug.find_by_name("news_and_events").model, @page]
     end
   end
 end
