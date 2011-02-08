f98dcefcc589c17c804aaa27276e357febb3c0a0
Ian Mooney <ian@yoomee.com>
2011-02-08 15:35:48 +0000

    Email nic when a topic is created.

--- client/app/models/topic.rb
+++ client/app/models/topic.rb
@@ -1,4 +1,6 @@
 Topic.class_eval do
+
+  after_create :notify_nic
   
   # always notifiy nic when anyone posts in the forum
   def notify_others_with_nic post
@@ -9,5 +11,13 @@ Topic.class_eval do
     notify_others_without_nic(post)
   end
   alias_method_chain :notify_others, :nic
+  
+  private
+  def notify_nic
+    if (member_nic = Member.nic) && (post = posts.first)
+      Notifier.deliver_post_notification(member_nic, post) unless post.owned_by?(member_nic)
+    end
+    true
+  end
   
 end
