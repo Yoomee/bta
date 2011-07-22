1c69e108d8e1784bf94d6af8d3767df518059782
Si Wilkins <si@yoomee.com>
2011-07-22 15:12:29 +0100

    Added referrer logging

--- client/app/controllers/documents_controller.rb
+++ client/app/controllers/documents_controller.rb
@@ -1,7 +1,12 @@
 DocumentsController.class_eval do
   
   def show_with_analytics
-    show_without_analytics if request.referrer =~ /tinnitus\.org\.uk/m
+    @logger = Logger.new("#{RAILS_ROOT}/log/document_referrers.log")
+    @logger.info "*******************"
+    @logger.info "TIME: #{Time.now}"
+    @logger.info "Document id = #{@document.id}"
+    @logger.info "Referrer = #{request.referrer}"
+    show_without_analytics if request.referrer =~ /tinnitus\.org\.uk/i
   end
   alias_method_chain :show, :analytics
   
