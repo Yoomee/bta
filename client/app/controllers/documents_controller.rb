3158d7dba9247ad5d9ba4559ae814dc782347b39
Si Wilkins <si@yoomee.com>
2011-07-22 15:34:03 +0100

    Added fix for when referrer isn't passed

--- client/app/controllers/documents_controller.rb
+++ client/app/controllers/documents_controller.rb
@@ -6,7 +6,8 @@ DocumentsController.class_eval do
     @logger.info "TIME: #{Time.now}"
     @logger.info "Document id = #{@document.id}"
     @logger.info "Referrer = #{request.referrer}"
-    show_without_analytics if request.referrer =~ /tinnitus\.org\.uk/i
+    # If the link has come from the website, or t is within the past minute
+    show_without_analytics if (request.referrer =~ /tinnitus\.org\.uk/i) || (params[:t] > 1.minute.ago.to_i)
   end
   alias_method_chain :show, :analytics
   
