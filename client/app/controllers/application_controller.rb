65d88119247a6bcdaea782bfedfd1a2f74e4ae5e
Si Wilkins <si@yoomee.com>
2011-07-22 15:53:57 +0100

    Change to email recipients

--- client/app/controllers/application_controller.rb
+++ client/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 ApplicationController.class_eval do
 
   ExceptionNotifier.email_prefix = 'BTA: '
+  ExceptionNotifier.exception_recipients = 'si@yoomee.com'
 
   skip_before_filter :force_password_change
 
