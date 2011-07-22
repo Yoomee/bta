bc4f6225eb68e23e83e777d2ddcf60fddfcc60f3
Si Wilkins <si@yoomee.com>
2011-07-22 15:46:01 +0100

    Change to redirect condition

--- client/app/controllers/documents_controller.rb
+++ client/app/controllers/documents_controller.rb
@@ -7,7 +7,7 @@ DocumentsController.class_eval do
     @logger.info "Document id = #{@document.id}"
     @logger.info "Referrer = #{request.referrer}"
     # If the link has come from the website, or t is within the past minute
-    show_without_analytics if (request.referrer =~ /tinnitus\.org\.uk/i) || (params[:t] && params[:t].to_i  > 1.minute.ago.to_i)
+    show_without_analytics if (request.referrer =~ /tinnitus\.org\.uk/i) || (params[:t] && (params[:t].to_i  > 1.minute.ago.to_i))
   end
   alias_method_chain :show, :analytics
   
