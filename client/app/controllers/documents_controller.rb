4846ef4b593317c33389141f16d9a6ccabc94b8f
Si Wilkins <si@yoomee.com>
2011-07-22 14:45:37 +0100

    Improved document tracking

--- client/app/controllers/documents_controller.rb
+++ client/app/controllers/documents_controller.rb
@@ -1,7 +1,8 @@
 DocumentsController.class_eval do
   
-  # def show
-  #   render :action =
-  # end
+  def show_with_analytics
+    show_without_analytics if request.referrer =~ /tinnitus\.org\.uk/m
+  end
+  alias_method_chain :show, :analytics
   
 end
