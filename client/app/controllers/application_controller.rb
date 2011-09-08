0a85ab5f20d37863dfe69a6b3ea5c83255ba5655
Si Wilkins <si@yoomee.com>
2011-09-08 09:03:29 +0100

    Disabled logging in for server move

--- client/app/controllers/application_controller.rb
+++ client/app/controllers/application_controller.rb
@@ -4,5 +4,19 @@ ApplicationController.class_eval do
   ExceptionNotifier.exception_recipients = 'developers@yoomee.com'
 
   skip_before_filter :force_password_change
+  
+  before_filter :log_out
+  
+  def log_out
+    puts "IN log_out"
+    puts "session[:logged_in_member_id] = #{session[:logged_in_member_id]}"
+    if session[:logged_in_member_id]
+      session[:logged_in_member_id] = nil
+      @logged_in_member = nil
+      flash.clear
+      flash[:notice] = "The BTA website is currently undergoing some maintenance, and logging in has been temporarily disabled. Please check back again later."
+      redirect_to root_url
+    end
+  end
 
 end
