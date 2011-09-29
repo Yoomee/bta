b926016f2eb9e6864addea24f15a13a03d9a2cab
Si Wilkins <si@yoomee.com>
2011-09-29 12:28:05 +0100

    Fix to deploy hook

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,2 +1,2 @@
 #run "cd #{current_path} && rake ts:rebuild RAILS_ENV=production -t"
-run "cd #{current_path} && whenever --update-crontab tinnitus"
+run "cd #{current_path} && whenever --update-crontab #{current_path}"
