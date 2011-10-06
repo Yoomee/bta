398fd8792d5c6a727f9164321ddcb364cab77162
Si Wilkins <si@yoomee.com>
2011-10-06 15:47:40 +0100

    Change to tweet method

--- client/app/controllers/home_controller.rb
+++ client/app/controllers/home_controller.rb
@@ -18,7 +18,7 @@ HomeController.class_eval do
   
   private
   def get_latest_tweet
-    @latest_tweet = latest_tweet_for("britishtinnitus")
+    @latest_tweet = latest_tweet_for("britishtinnitus").text
   end
   
 end
