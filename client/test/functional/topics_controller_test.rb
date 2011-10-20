9af2278f9f9968d342bb4a9cc24fa715ca96fc27
Ian Mooney <ian@yoomee.com>
2011-10-20 15:51:14 +0100

    Permissions for topics#show for private forums.

--- client/test/functional/topics_controller_test.rb
+++ client/test/functional/topics_controller_test.rb
@@ -1,6 +1,18 @@
 require File.dirname(__FILE__) + '/../../../test/test_helper'
 class TopicsControllerTest < ActionController::TestCase
   
-  should have_action(:show).with_level(:member_only)
+  context "on call to allowed_to? with show" do
+  
+    setup do
+      @member = Factory.build(:member)
+      @topic = Factory.build(:topic, :id => 123)
+      Topic.stubs(:find).with(123).returns @topic
+    end
+
+    should 'return false if logged out' do
+      assert !TopicsController.allowed_to?({:action => 'show', :id => @topic.id}, nil)
+    end
+    
+  end
   
 end
