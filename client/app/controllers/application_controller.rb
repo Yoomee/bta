6c0775b0e99400cc952c852884d7249cc5eec22d
Matt Atkins <matt@yoomee.com>
2012-12-04 17:27:05 +0000

    Log multiple IP addresses

--- client/app/controllers/application_controller.rb
+++ client/app/controllers/application_controller.rb
@@ -8,6 +8,38 @@ ApplicationController.class_eval do
   
   #before_filter :log_out
   
+  def login_member!(member, options = {})
+    options.reverse_merge!(:redirect => true, :super => false, :remember_me => false)
+    @logged_in_member = member
+    store_remember_me_cookies(@logged_in_member) if options[:remember_me]
+    session[:logged_in_member_id] = @logged_in_member.id
+    unless options[:super] || request.remote_ip.blank?
+      ip_addresses = @logged_in_member.ip_address.to_s.split(',')
+      ip_addresses << request.remote_ip
+      @logged_in_member.update_attribute(:ip_address, ip_addresses.uniq.sort.join(','))
+    end
+    if Module.value_to_boolean(params[:in_popup])
+      render(:text => "<script type='text/javascript'>window.close()</script>")
+    elsif options[:redirect]
+      options.reverse_merge!(:flash_notice => "Welcome back #{@logged_in_member.forename}! Thanks for logging in again.")
+      flash[:notice] = options[:flash_notice] if options[:flash_notice].present?
+      return redirect_to(params[:redirect_to]) if !params[:redirect_to].nil?
+      if params[:iframe] || options[:iframe]
+        responds_to_parent do
+          render :update do |page|
+            redirect_url = login_waypoint || waypoint || root_url
+            if redirect_url.is_a?(Hash) && redirect_url.keys.collect(&:to_s).include?("anchor")
+              redirect_url[:reload] = true
+            end
+            page.redirect_to(redirect_url)
+          end
+        end
+      else
+        redirect_to_waypoint_after_login
+      end
+    end
+  end
+  
   def log_out
     puts "IN log_out"
     puts "session[:logged_in_member_id] = #{session[:logged_in_member_id]}"
--- client/app/models/members_report.rb
+++ client/app/models/members_report.rb
@@ -2,16 +2,20 @@ MembersReport.class_eval do
   
   class << self
     def field_names
-      [:email, :username, :password, :surname, :forename, :created_at, :updated_at]
+      [:email, :username, :password, :surname, :forename, :ip_address, :created_at, :updated_at]
     end
   
     def headings
-      ["Email", "Username", "Password", "Surname", "Forename", "Created At", "Updated At"]
+      ["Email", "Username", "Password", "Surname", "Forename", "IP Addresses", "Created At", "Updated At"]
     end
   end
 end
   
 MembersReport::Row.class_eval do
+  
+    def ip_address
+      @member.ip_address
+    end
     
     def password
       @member.password
