6f2ff145b2271b7ae4e7c45bc5db886c5ce32d8e
Matt Atkins <matt@yoomee.com>
2013-01-15 12:35:40 +0000

    cc inappropriate content reports to Nic

--- client/app/models/notifier.rb
+++ client/app/models/notifier.rb
@@ -1,5 +1,21 @@
 Notifier.class_eval do
   
+  def enquiry_notification enquiry
+    site_name = APP_CONFIG['site_name']
+    site_url = APP_CONFIG['site_url']
+    recipients enquiry.email_to
+    from enquiry.email_from
+    bcc "support@yoomee.com"
+    if enquiry.form_name == "report"
+      cc Member.nic.email
+    end
+    subject enquiry.email_subject
+    content_type  "multipart/alternative"
+    locals = {:enquiry => enquiry, :site_name => site_name, :site_url => site_url}
+    part :content_type => "text/plain", :body => render_message("enquiry_notification.text.plain", locals)
+    part :content_type => "text/html", :body => render_message("enquiry_notification.text.html", locals)
+  end
+  
   def post_notification member, post
     recipients member.email
     from APP_CONFIG[:site_email]
