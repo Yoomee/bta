5c3f034a4a124f395ac6ea8f82cbc08e53694972
Matt Atkins <matt@yoomee.com>
2013-01-15 09:44:57 +0000

    Order posts by latest on member#show, add hr

--- client/app/controllers/members_controller.rb
+++ client/app/controllers/members_controller.rb
@@ -1,5 +1,5 @@
 MembersController.class_eval do
   def show
-    @posts = @member.posts.paginate(:page => params[:page])
+    @posts = @member.posts.latest.paginate(:page => params[:page])
   end
 end
