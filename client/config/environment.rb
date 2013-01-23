0ff7e6ec01248c74632e6d4f621e2255993b61da
Matt Atkins <matt@yoomee.com>
2013-01-23 16:21:57 +0000

    Add mogli and ancestry to config.gem

--- client/config/environment.rb
+++ client/config/environment.rb
@@ -3,6 +3,8 @@ class ClientEnvironment
   class << self
     def setup(config)
       config.gem 'yoomee-searchlogic', :lib => 'searchlogic'
+      config.gem 'mogli'
+      config.gem 'ancestry'
       config.gem 'facebooker2'       
     end
   end
