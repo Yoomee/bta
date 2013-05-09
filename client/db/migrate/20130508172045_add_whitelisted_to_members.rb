ca0bcc7ef34e2f5db931c800e85623e511bf8ef3
Matt Atkins <matt@yoomee.com>
2013-05-09 15:20:59 +0100

    Ensure admins aren't banned from the forum

--- client/db/migrate/20130508172045_add_whitelisted_to_members.rb
+++ client/db/migrate/20130508172045_add_whitelisted_to_members.rb
@@ -3,7 +3,7 @@ class AddWhitelistedToMembers < ActiveRecord::Migration
     add_column :members, :whitelisted, :boolean, :default => false
     Member.reset_column_information
     Member.find(:all, :conditions => {:is_admin => true}).each  do |member|
-      member.update_attribute(:whitelisted,true)
+      member.update_attributes(:whitelisted => true, :banned_from_forum => false)
     end
   end
 
