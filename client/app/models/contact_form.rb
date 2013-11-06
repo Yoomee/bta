899830d32fd836fb0b56628b9ed87ed647aaf325
Ian Mooney <ian@yoomee.com>
2013-11-06 16:21:49 +0000

    Add phone to contact_form, checkboxes for 'more information on'

--- client/app/models/contact_form.rb
+++ client/app/models/contact_form.rb
@@ -3,7 +3,7 @@ module ContactForm
   include EnquiryForm
   
   title "Contact Us"
-  fields :name, :email_address, :address, :i_would_like_more_information_because, :please_send_me_more_information_on, :please_send_this_information_to_me, :i_would_like_to_receive_regular_email_updates_from_the_bta, :message
+  fields :name, :email_address, :address, :phone_number, :i_would_like_more_information_because, :please_send_me_more_information_on, :please_send_this_information_to_me, :i_would_like_to_receive_regular_email_updates_from_the_bta, :message
   
   email_to "info@tinnitus.org.uk"
   email_from "website@tinnitus.org.uk"
@@ -11,7 +11,7 @@ module ContactForm
   
   response_message "Thank you for your enquiry. We will be in touch shortly."
   
-  validates_presence_of :name, :email_address, :address, :message, :i_would_like_more_information_because, :please_send_me_more_information_on, :please_send_this_information_to_me
+  validates_presence_of :name, :email_address, :address, :phone_number, :message, :i_would_like_more_information_because, :please_send_me_more_information_on, :please_send_this_information_to_me
   validates_format_of :email_address, :with => ValidateExtensions::EMAIL_FORMAT
   
 end
